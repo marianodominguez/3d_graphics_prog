6b444fa8767daeb6505ef64ce7d2729d65d93cc0
Mariano Dominguez <mariano.dm@gmail.com>
2024-01-30 10:33:43 -0800

    working lighting normals not correct

--- gl/src/cube.c
+++ gl/src/cube.c
@@ -1,6 +1,7 @@
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
 #include "linmath.h"
 #include <math.h>
 #include <stdlib.h>
@@ -8,7 +9,7 @@
 
 #define nvertices 3*12
 
-vec3 vertices[nvertices] =
+static const vec3 vertices[nvertices] =
 {
     {-1.0f,-1.0f,-1.0f}, // triangle 1 : begin
     {-1.0f,-1.0f, 1.0f},
@@ -47,12 +48,14 @@ vec3 vertices[nvertices] =
     {-1.0f, 1.0f, 1.0f},
     {1.0f,-1.0f, 1.0f}
 };
+
 static vec3 normals[nvertices];
+static vec3 LightPosition  = (vec3){5.0f, 5.0f, 10.0f};
 
 static const char* vertex_shader_text =
 "#version 330 core\n"
-"attribute vec3 vPos;\n"
 "attribute vec3 vNormal;\n"
+"attribute vec3 vPos;\n"
 "out vec3 FragPos;\n"
 "out vec3 Normal;\n"
 "uniform mat4 M;\n"
@@ -61,25 +64,23 @@ static const char* vertex_shader_text =
 "void main()\n"
 "{\n"
 "    FragPos =vec3( M * vec4(vPos, 1.0) );\n"
-"    Normal=vNormal;\n"
-"    gl_position = P *V * vec4(FragPos, 1.0);\n"
+"    Normal = vNormal;\n"
+"    gl_Position = P*V*vec4(FragPos, 1.0);\n"
 "}\n";
 
 static const char* fragment_shader_text =
 "#version 330 core\n"
-"out vec4 FragColor;\n"
 "in vec3 Normal;\n"
 "in vec3 FragPos;\n"
 "uniform vec3 lightPos;\n"
 "void main()\n"
 "{\n"
+"    vec3 ambient = vec3(0.3,0.3,0.3);\n"
 "    vec3 norm = normalize(Normal);\n"
 "    vec3 lightDir = normalize(lightPos - FragPos);\n"
 "    float diff = max(dot(norm, lightDir), 0.0);\n"
-"    vec3 diffuse = diff * vec3(1.0,1.0,1.0);\n"
-"    vec3 result = diffuse;\n"
-"    FragColor = vec4(result, 1.0);\n"
-"    //FragColor = vec3(0.5,1.0,1.0);"
+"    vec3 color = diff*vec3(1.0,1.0,1.0)+ambient;\n"
+"    gl_FragColor = vec4(color, 1.0);\n"
 "}\n";
 
 static void error_callback(int error, const char* description)
@@ -93,29 +94,36 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
         glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
 
-void normalMatrix() {
-    for(int i=0; i<nvertices-1; i++) {
-        vec3_mul_cross(normals[i], vertices[i],vertices[i+1]);
+void makeNormalMatrix() {
+    for(int i=0; i<nvertices; i+=3) {
+        vec3 ab,ac,bc,ba;
+        vec2_sub(ab,normals[i+1], vertices[i]);
+        vec2_sub(ac,normals[i+2], vertices[i]);
+        vec2_sub(bc,normals[i+2], vertices[i+1]);
+        vec2_sub(ba,normals[i], vertices[i+1]);
+
+        vec3_mul_cross(normals[i],ac,ab);
+        vec3_mul_cross(normals[i+1],bc,ba);
+        vec3_mul_cross(normals[i+2],ac,bc);
+
     }
     vec3_mul_cross(normals[nvertices-1],vertices[nvertices-1], vertices[0]);
 }
 
 int main(void)
 {
-    normalMatrix();
+    makeNormalMatrix();
     GLFWwindow* window;
     GLuint vertex_buffer, vertex_shader, fragment_shader, program;
-    GLint vpos_location, vcol_location,m_location,v_location,p_location;
-    unsigned int cubeVAO;
+    GLint p_location,v_location,m_location, vpos_location, vnormal_location,light_location;
 
     glfwSetErrorCallback(error_callback);
 
     if (!glfwInit())
         exit(EXIT_FAILURE);
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
     window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
     if (!window)
@@ -130,11 +138,12 @@ int main(void)
     glewInit();
     glfwSwapInterval(1);
 
-    glGenVertexArrays(1, &cubeVAO);
+    // NOTE: OpenGL error checks have been omitted for brevity
+
+    //glEnable(GL_DEPTH_TEST);
     glGenBuffers(1, &vertex_buffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glBindVertexArray(cubeVAO);
 
     vertex_shader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
@@ -153,23 +162,21 @@ int main(void)
     v_location = glGetUniformLocation(program, "V");
     p_location = glGetUniformLocation(program, "P");
     vpos_location = glGetAttribLocation(program, "vPos");
-    GLint normal_location = glGetAttribLocation(program, "vNormal");
-    GLint light_location = glGetUniformLocation(program, "lightPos");
+    vnormal_location = glGetAttribLocation(program, "vNormal");
+    light_location = glGetUniformLocation(program, "lightPos");
 
     glEnableVertexAttribArray(vpos_location);
     glVertexAttribPointer(vpos_location, 3, GL_FLOAT, GL_FALSE,
                           sizeof(vertices[0]), (void*) 0);
-    glEnableVertexAttribArray(normal_location);
-    glVertexAttribPointer(normal_location, 3, GL_FLOAT, GL_FALSE,
-                          sizeof(normals[0]), (void*) 0);
-
-    static vec3 LightPosition  = (vec3){5.0f, 5.0f, 10.0f};
+    glEnableVertexAttribArray(vnormal_location);
+    glVertexAttribPointer(vnormal_location, 3, GL_FLOAT, GL_FALSE,
+                          sizeof(normals[0]), (void*) 0 );
 
     while (!glfwWindowShouldClose(window))
     {
         float ratio;
         int width, height;
-        mat4x4 m,p,mvp,v,mv;
+        mat4x4 m, p, mvp,v;
 
         glfwGetFramebufferSize(window, &width, &height);
         ratio = width / (float) height;
@@ -179,28 +186,33 @@ int main(void)
 
         mat4x4_identity(m);
         mat4x4_rotate_Z(m, m, (float) glfwGetTime());
+        //mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+
         mat4x4_perspective(p, M_PI/2 , (float) width / (float)height, 0.1f, 100.0f);
 
         // Camera matrix
         mat4x4_look_at(v,
-            (vec3){4,3,3}, // Camera is at (4,3,3), in World Space
-            (vec3){0,0,0}, // and looks at the origin
-            (vec3){0,1,0}  // Head is up (set to 0,-1,0 to look upside-down)
+        (vec3){3,2,2}, // Camera in World Space
+        (vec3){0,0,0}, // and looks at the origin
+        (vec3){0,1,0}  // Head is up (set to 0,-1,0 to look upside-down)
         );
-        mat4x4_mul(mv, v, m);
-        mat4x4_mul(mvp, p, mv);
+        mat4x4_mul(mvp, v, m);
+        mat4x4_mul(mvp, p, mvp);
+
 
         glUseProgram(program);
         glUniformMatrix4fv(m_location, 1, GL_FALSE, (const GLfloat*) m);
         glUniformMatrix4fv(v_location, 1, GL_FALSE, (const GLfloat*) v);
         glUniformMatrix4fv(p_location, 1, GL_FALSE, (const GLfloat*) p);
         glUniform3fv(light_location,1,(const GLfloat*) LightPosition);
-
         glDrawArrays(GL_TRIANGLES, 0, 3*12);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
     glfwDestroyWindow(window);
+
     glfwTerminate();
     exit(EXIT_SUCCESS);
 }
