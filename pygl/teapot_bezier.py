f217d8df24a49c688358c028843b9c6c7f5cb680
Mariano Dominguez <mariano.dm@gmail.com>
2024-02-11 21:04:34 -0800

    port teapot to python

--- pygl/teapot_bezier.py
+++ pygl/teapot_bezier.py
@@ -4,6 +4,8 @@ import numpy as np
 import glfw
 import sys
 from OpenGL.GL import *
+import glm
+import math
 
 strVertexShader = """
 #version 330
@@ -28,16 +30,11 @@ void main()
 
 program=None
 vertex_buffer=None
+vp_size_changed=False
 
 #TODO use model
-nvertices=3
+nvertices=16*32
 
-vertices = np.array(
-    [0.75, 0.75, 0.0, 1.0,
-    0.75, -0.75, 0.0, 1.0,
-    -0.75, -0.75, 0.0, 1.0],
-    dtype='float32'
-)
 
 def createShader(shaderType, shaderFile):
     shader = glCreateShader(shaderType)
@@ -50,7 +47,7 @@ def createShader(shaderType, shaderFile):
     if status == GL_FALSE:
         # Note that getting the error log is much simpler in Python than in C/C++
         # and does not require explicit handling of the string buffer
-        strInfoLog = glGetShaderInforLog(shader)
+        strInfoLog = glGetShaderInfoLog(shader)
         strShaderType = ""
         if shaderType is GL_VERTEX_SHADER:
             strShaderType = "vertex"
@@ -76,25 +73,34 @@ def load_model(filename):
     nvertices=int(f.readline())
     for i in range(nvertices):
         line=f.readline()
-        vertices.append( np.array([float(p) for p in line.split(',')]) )
+        vertices.append( glm.vec3([float(p) for p in line.split(',')]) )
 
     return {"patches":patches,"vertices":vertices};
 
 def draw():
     #global vertex_buffer,nvertices,program
+    width = glfw.get_framebuffer_size(window)[0]
+    height = glfw.get_framebuffer_size(window)[1]
+    ratio = width / height;
+
+    glViewport(0, 0, width, height);
+
     glClearColor(0.0, 0.0, 0.0, 0.0)
-    glClear(GL_COLOR_BUFFER_BIT)
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glUseProgram(program)
 
     glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer)
     glEnableVertexAttribArray(0)
     glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, None)
 
+    p=glm.perspective(math.pi / 2, ratio, 0.1, 50.0)
+
     glDrawArrays(GL_TRIANGLES, 0, nvertices)
 
-    glDisableVertexAttribArray(0)
-    glUseProgram(0)
 
+def resize_cb(window, w, h):
+    global vp_size_changed
+    vp_size_changed = True
 
 def init():
     if not glfw.init():
@@ -108,11 +114,27 @@ def init():
 
     # Make the window's context current
     glfw.make_context_current(window)
+    glfw.set_window_size_callback(window, resize_cb)
     return window
 
-#m=load_model("../models/teapot")
+def generate_patches(model):
+    result=[]
+    for p in model['patches']:
+        #print(p)
+        for i in range(16):
+            result.append(model['vertices'][p[i]-1])
+        #result.append(v)
+    return np.array(result)
+
+m=load_model("../models/teapot")
 #print(m['patches'])
 #print(m['vertices'])
+
+patches = generate_patches(m)
+np.set_printoptions(floatmode="maxprec", precision=4)
+for p in patches:
+    print(p)
+
 window=init()
 
 shaderList = []
@@ -126,6 +148,7 @@ for shader in shaderList:
     glAttachShader(program, shader)
     glLinkProgram(program)
 
+
 for shader in shaderList:
     glDeleteShader(shader)
 
@@ -134,12 +157,23 @@ vertex_buffer = glGenBuffers(1)
 glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer)
 glBufferData( # PyOpenGL allows for the omission of the size parameter
     GL_ARRAY_BUFFER,
-    vertices,
+    patches,
     GL_STATIC_DRAW
 )
-glBindBuffer(GL_ARRAY_BUFFER, 0)
+
+vpos_location=glGetAttribLocation(program, 'position')
+glEnableVertexAttribArray(vpos_location);
+glVertexAttribPointer(vpos_location, 3, GL_FLOAT, GL_FALSE,
+                           0,  None)
+glBindBuffer(GL_ARRAY_BUFFER, vpos_location)
 glBindVertexArray(glGenVertexArrays(1))
 
+
+#setup camera
+# Camera matrix
+v =glm.lookAt(glm.vec3(10, 10, 10), # Camera in World Space
+            glm.vec3(0, 0, 0),  # and looks at the origin
+            glm.vec3(0, 1, 0));
 
 while not glfw.window_should_close(window):
     # Render here, e.g. using pyOpenGL
@@ -149,5 +183,10 @@ while not glfw.window_should_close(window):
 
     # Poll for and process events
     glfw.poll_events()
+    if vp_size_changed:
+        vp_size_changed = False
+        w, h = glfw.get_framebuffer_size(window)
+        glViewport(0, 0, w, h)
+        #print("new viewport size:", w, h)
 
 glfw.terminate()
