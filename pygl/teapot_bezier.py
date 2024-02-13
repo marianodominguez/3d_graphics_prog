bd8fefd3815e47f0bc7a84d0b528118f13b327a9
Mariano Dominguez <marianod@wepay.com>
2024-02-13 10:30:59 -0800

    python impl working in mac

--- pygl/teapot_bezier.py
+++ pygl/teapot_bezier.py
@@ -133,9 +133,9 @@ def init():
     # Create a windowed mode window and its OpenGL context
 
     glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
-    glfw.window_hint(glfw.CONTEXT_VERSION_MINOR,0)
-    #glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, GL_TRUE)
-    #glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
+    glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
+    glfw.window_hint(glfw.OPENGL_FORWARD_COMPAT, GL_TRUE)
+    glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
     glfw.window_hint(glfw.OPENGL_DEBUG_CONTEXT, GL_TRUE)
 
     window = glfw.create_window(640, 480, "Utah teapot", None, None)
