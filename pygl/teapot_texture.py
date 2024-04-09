b084086cd7120a939c37d1e5214d3cf86fcf1926
Mariano Dominguez <mariano.dm@gmail.com>
2024-04-08 20:38:07 -0700

    debug black texture

--- pygl/teapot_texture.py
+++ pygl/teapot_texture.py
@@ -38,13 +38,12 @@ def read_texture(filename):
     glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
 
     glBindTexture(GL_TEXTURE_2D, textID)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
 
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img.size[0], img.size[1], 0, GL_RGB, GL_UNSIGNED_BYTE, img_data)
-    glBindTexture(GL_TEXTURE_2D, 0)
     return textID
 
 def createShader(shaderType, shaderFile):
